3896915674b92cb2cdac2e9872500ebd29f3b2ba
dongdaokuan <dongdaokuan@163.com>
2026-03-01 20:09:37 +0800

    Initial Commit

---
+++ woai/woaiApp.swift
@@ -0,0 +1,17 @@
+//
+//  woaiApp.swift
+//  woai
+//
+//  Created by 润泽 on 2026/3/1.
+//
+
+import SwiftUI
+
+@main
+struct woaiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
